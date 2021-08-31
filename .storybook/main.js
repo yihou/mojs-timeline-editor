34bd647909a8de9ea093e641a854f0ea96440353
Yi Hou <looyihou91@gmail.com>
2021-08-31 17:57:46 +0800

    add JsDocs for webpack configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,37 @@
-const path = require("path");
-const fs = require("fs");
+const path = require("path")
+const fs = require("fs")
 
 function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath));
+  let currDir = path.dirname(require.resolve(filepath))
   while (true) {
     if (fs.existsSync(path.join(currDir, "package.json"))) {
-      return currDir;
+      return currDir
     }
-    const { dir, root } = path.parse(currDir);
+    const { dir, root } = path.parse(currDir)
     if (dir === root) {
       throw new Error(
         `Could not find package.json in the parent directories starting from ${filepath}.`
-      );
+      )
     }
-    currDir = dir;
+    currDir = dir
   }
 }
 
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ["../src/**/*.stories.tsx"],
+  /**
+   *
+   * @param {webpack.Configuration} config
+   * @return {Promise<*>}
+   */
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       "@emotion/core": getPackageDir("@emotion/react"),
       "@emotion/styled": getPackageDir("@emotion/styled"),
-      "emotion-theming": getPackageDir("@emotion/react"),
+      "emotion-theming": getPackageDir("@emotion/react")
     }
 
     return config
-  },
-};
+  }
+}
